caf227db7f320502cc9fddde882740bd46231699
Carlos Guevara Ram <carlosguevaracontacto@gmail.com>
2026-01-04 17:23:56 -0600

    Fixed vite proxy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
+import { getStandings } from "./services/api";
+import type { TeamStanding } from "./types/football";
+
 function App() {
+  const [standings, setStanding] = useState<TeamStanding[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getStandings('PL')
+        setStanding(data);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : 'Unknown error');
+      } finally {
+        setLoading(true);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) return <div className="p-8 text-white">Loading....</div>
+  if (error) return <div className="p-8 text-red-500">Error: {error} </div>
 
   return (
     <div className="min-h-screen bg-gray-900 text-white p-8">
-      <h1 className="text-3xl font-bold">Soccer Analytics</h1>
-      <p className="text-gray-200 mt-2">Let's build it</p>
+      <h1 className="text-3xl font-bold mb-6">Premier League Standings</h1>
+      <pre className="bg-gray-800 p-4 rounded overflow-auto text-sm">
+        {JSON.stringify(standings.slice(0,3), null, 2)}
+      </pre>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import { env } from "process";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api/football": {
-        target: "https://api.football-data.org",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/football/, ""),
-        headers: {
-          "X-Auth-Token": env.VITE_FOOTBALL_DATA_API_KEY ?? "",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api/football": {
+          target: "https://api.football-data.org",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/football/, ""),
+          headers: {
+            "X-Auth-Token": env.VITE_FOOTBALL_DATA_API_KEY ?? "",
+          },
         },
       },
     },
-  },
+  };
 });
